90c5ccc0793bd02db3720ad4ec1cae7a7f739333
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-06-26 18:50:21 -0500

    add: services layer

---
+++ service/ModelServices.cs
@@ -0,0 +1,33 @@
+﻿using data.repository.interfaces;
+using GlobalGO.models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace service
+{
+    public partial class ModelServices
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ModelServices(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Modelos>> getAll()
+        {
+            try
+            {
+                List<Modelos> listModels = _unitOfWork.modelRepository.getModels();
+                return listModels;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
